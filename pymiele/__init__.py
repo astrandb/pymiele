44e2bb335995de7c87c5c807f1fd068a7593500d
Åke Strandberg <ake@strandberg.eu>
2024-11-12 09:29:31 +0100

    Export __version__

--- pymiele/__init__.py
+++ pymiele/__init__.py
@@ -1,3 +1,5 @@
 """Library for Miele integration with Home Assistant."""
+
 from .const import *  # noqa: F401, F403
+from .const import VERSION as __version__  # noqa: F401
 from .pymiele import *  # noqa: F401, F403
