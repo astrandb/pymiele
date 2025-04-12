cd8c02795de8be644a58de271eac6fa834f9b5dc
Åke Strandberg <ake@strandberg.eu>
2025-04-12 13:35:42 +0200

    Add and fix models and add setters

--- pymiele/const.py
+++ pymiele/const.py
@@ -1,7 +1,9 @@
 """Constants for pymiele."""
 
-VERSION = "0.3.1"
+VERSION = "0.3.2"
 
 MIELE_API = "https://api.mcs3.miele.com/v1"
 OAUTH2_AUTHORIZE = "https://api.mcs3.miele.com/thirdparty/login"
 OAUTH2_TOKEN = "https://api.mcs3.miele.com/thirdparty/token"
+
+AIO_TIMEOUT = 15
